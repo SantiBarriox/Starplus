f5ef11e3b9c8ebae59fa9ef292d9e37c39a726c5
Alexis Mauricio Gunsett <mau_gunsett@hotmail.com>
2023-09-16 10:50:54 -0300

    agrego logica de abm

---
+++ js/admin/abm.js
@@ -0,0 +1,95 @@
+import { PeliculaSeries } from "./PeliculasSeries.js";
+import { agregarPeliculasOSeriesALS, cargarTabla, obtenerPeliculasOSeriesDeLS } from "./adminUtils.js";
+
+
+export const añadirPeliculasSerie = (nombre, tipo, duracion, caratula, descripcion, publicada) =>{
+    const nuevaPOS = new PeliculaSeries (nombre, tipo, duracion, caratula, descripcion, publicada)
+
+agregarPeliculasOSeriesALS(nuevaPOS);
+
+    swal.fire({
+        title:"Exito",
+        text: "Se guardo de manera exitosa",
+        icon:"success",
+    })
+};
+
+ 
+
+
+export const editarPeliculasSeries = (nombre, tipo, duracion, caratula, descripcion, publicada) =>{
+    // traer lista y el codigo 
+const codigo = sessionStorage.getItem("codigoPeliculaOSerie")
+const peliculaOSerie = obtenerPeliculasOSeriesDeLS();
+// si no hay codigo es null
+if(!codigo){
+    swal.fire({
+        title: "Error",
+        text:"No se encontro",
+        icon: "Error",
+    })
+    return;
+} 
+// bucar pelicula
+const posicionpeliculaOSerie = peliculaOSerie.findIndex(
+    (item)=>item.codigo === codigo
+    );
+if(posicionpeliculaOSerie === -1){
+    swal.fire({
+        title: "Error",
+        text:"No se encontro",
+        icon: "Error",
+    })
+}
+// crear el editado
+const pelicilaOSerieEditada = new PeliculaSeries(nombre,tipo,duracion,caratula,descripcion,publicada)
+// eliminar el anterior y agregar el nuevo
+peliculaOSerie.splice(posicionpeliculaOSerie,1,pelicilaOSerieEditada)
+// guardar en el ls
+
+localStorage.setItem("peliculasSeries", JSON.stringify(peliculaOSerie))
+swal.fire({
+    title: "Exito",
+    text:"Se modifico con exito",
+    icon: "success",
+})
+
+// resetear estado previo a edicion
+
+sessionStorage.removeItem("codigoPeliculaOSerie")
+}
+
+
+export const eliminarPeliculasSerie = (codigo) => {
+//    confirmar
+swal
+.fire({
+    title:"¿Estas seguro?",
+    text:"Esta opción no será reversible",
+    icon:"warning",
+    showCancelButton: true,
+    cancelButtonText:"Cancelar",
+    confirmButtonText:"Si, eliminar",
+    })
+    .then((action)=>{
+        if(action.isConfirmed){
+            
+            // trer lista
+            const peliculaOSerie = obtenerPeliculasOSeriesDeLS();
+
+            const listaFiltrada = peliculaOSerie.filter(
+                (item)=>item.codigo !== codigo
+                );
+                localStorage.setItem("peliculasSeries", JSON.stringify(listaFiltrada))
+
+                swal.fire({
+                    title: "Exito",
+                    text:"Se elimino con exito",
+                    icon: "success",
+                })
+                // recargar tabla
+                cargarTabla();
+
+        } 
+    });
+}
